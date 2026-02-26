ce6d22d01662a80c4379e34313cef86646393579
bystrytska <kate.bystrytska@gmail.com>
2026-02-26 01:28:20 +0000

    Deploying to gh-pages from @ bystrytska/bystrytska.github.io@9b0a56c10a3c5c7eab9af7386f003653287ff7e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -428,12 +428,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+      },{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
